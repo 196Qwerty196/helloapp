aabb6caea2d305cd3dff8484efd8c960b0fb66f1
Burov_Ilya <burov.ilya96@gmail.com>
2026-07-10 18:33:52 +0500

    Create HelloApp

---
+++ HelloApp/HelloApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("hello");
+        }
+    }
+        
+}
+
+
+
